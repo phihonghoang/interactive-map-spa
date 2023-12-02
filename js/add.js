3908229e5eaf7e7d50942735e5d4e19ba8d06774
Phi Hong Hoang <phihonghoang@protonmail.com>
2023-12-02 22:30:34 +0100

    add.js and du.js updating an address works more than once.

--- js/add.js
+++ js/add.js
@@ -20,7 +20,6 @@ function addAddress(e) {
     let lon = document.getElementById("add-lon-id").value;
 
     addAddressToContainer(name,description,street,zip,city,state,lat,lon);
-
     emptyAddAddress();
 }
 
@@ -32,16 +31,64 @@ function addAddressToContainer(name,description,street,zip,city,state,lat,lon) {
                         street + " " + zip + " " + city + " " +
                         state + " " + lat + " " + lon + "<br><br>";
 
+    address.setAttribute('data-name', name);
+    address.setAttribute('data-description', description);
+    address.setAttribute('data-street', street);
+    address.setAttribute('data-zip', zip);
+    address.setAttribute('data-city', city);
+    address.setAttribute('data-state', state);
+    address.setAttribute('data-lat', lat);
+    address.setAttribute('data-lon', lon);
+
+    /*
+    let name = address.getAttribute('data-name');
+    let description = address.getAttribute('data-description');
+    let street = address.getAttribute('data-street');
+    let zip = address.getAttribute('data-zip');
+    let city = address.getAttribute('data-city');
+    let state = address.getAttribute('data-state');
+    let lat = address.getAttribute('data-lat');
+    let lon = address.getAttribute('data-lon');
+    */
+
     // the anonymous function acts as a wrapper.
     // this enables the expected delay in the execution of the function until the event.
     address.addEventListener("click", function(){
-        addToDu(name,description,street,zip,city,state,lat,lon);
+        addToDuValues(address.getAttribute('data-name'),
+        address.getAttribute('data-description'),
+        address.getAttribute('data-street'),
+        address.getAttribute('data-zip'),
+        address.getAttribute('data-city'),
+        address.getAttribute('data-state'),
+        address.getAttribute('data-lat'),
+        address.getAttribute('data-lon'));
+        addToDu();
 
-        // chosen address will be assigned to selectedAddress
+        // both variables are sharing the same object.
+        // it's a reference, any changes made in selectedAddress applies to address too.
         selectedAddress = address;
     });
+    
 
     document.getElementById("main-map-address-container-id").appendChild(address);
+}
+
+function addToDuValues(name,description,street,zip,city,state,lat,lon) {
+    document.getElementById("du-name-id").value = name;
+    document.getElementById("du-description-id").value = description;
+    document.getElementById("du-street-id").value = street;
+    document.getElementById("du-zip-id").value = zip;
+    document.getElementById("du-city-id").value = city;
+    document.getElementById("du-state-id").value = state;
+    document.getElementById("du-lat-id").value = lat;
+    document.getElementById("du-lon-id").value = lon;
+}
+
+function addToDu() {
+    document.getElementById("login-container-id").style.display = "none";
+    document.getElementById("main-container-id").style.display = "none";
+    document.getElementById("add-container-id").style.display = "none";
+    document.getElementById("delete-update-container-id").style.display = "";
 }
 
 function emptyAddAddress() {
@@ -54,23 +101,6 @@ function emptyAddAddress() {
     document.getElementById("add-state-id").value = "";
     document.getElementById("add-lat-id").value = "";
     document.getElementById("add-lon-id").value = "";
-}
-
-function addToDu(name,description,street,zip,city,state,lat,lon) {
-    document.getElementById("du-name-id").value = name;
-    document.getElementById("du-description-id").value = description;
-    document.getElementById("du-street-id").value = street;
-    document.getElementById("du-zip-id").value = zip;
-    document.getElementById("du-city-id").value = city;
-    document.getElementById("du-state-id").value = state;
-    document.getElementById("du-lat-id").value = lat;
-    document.getElementById("du-lon-id").value = lon;
-
-    document.getElementById("login-container-id").style.display = "none";
-    document.getElementById("main-container-id").style.display = "none";
-    document.getElementById("add-container-id").style.display = "none";
-    document.getElementById("delete-update-container-id").style.display = "";
-
 }
 
 function addToMain() {
--- js/du.js
+++ js/du.js
@@ -24,14 +24,29 @@ function duUpdate() {
     let state = document.getElementById("du-state-id").value;
     let lat = document.getElementById("du-lat-id").value;
     let lon = document.getElementById("du-lon-id").value;
-    
+
+    selectedAddress.setAttribute('data-name', name);
+    selectedAddress.setAttribute('data-description', description);
+    selectedAddress.setAttribute('data-street', street);
+    selectedAddress.setAttribute('data-zip', zip);
+    selectedAddress.setAttribute('data-city', city);
+    selectedAddress.setAttribute('data-state', state);
+    selectedAddress.setAttribute('data-lat', lat);
+    selectedAddress.setAttribute('data-lon', lon);
 
     selectedAddress.innerHTML = name + " " + description + " " + 
-                                street + " " + zip + " " + city + " " + 
-                                state + " " + lat + " " + lon + "<br><br>";
-    
+                        street + " " + zip + " " + city + " " + 
+                        state + " " + lat + " " + lon + "<br><br>";
+      
+            
     duToMain();
 }
+
+/*
+function hcaUpdate(name,description,street,zip,city,state,lat,lon) {
+    
+}
+*/
 
 function duDelete() {
     selectedAddress.parentNode.removeChild(selectedAddress);
@@ -44,4 +59,16 @@ function duToMain() {
     document.getElementById("main-container-id").style.display = "";
     document.getElementById("add-container-id").style.display = "none";
     document.getElementById("delete-update-container-id").style.display = "none";
+}
+
+function emptyDuAddress() {
+    // Empty the input fields.
+    document.getElementById("du-name-id").value = "";
+    document.getElementById("du-description-id").value = "";
+    document.getElementById("du-street-id").value = "";
+    document.getElementById("du-zip-id").value = "";
+    document.getElementById("du-city-id").value = "";
+    document.getElementById("du-state-id").value = "";
+    document.getElementById("du-lat-id").value = "";
+    document.getElementById("du-lon-id").value = "";
 }
