9c172d414a43d68a71c2b24d854a3b86d2653a67
LeonZeidler <147526502+LeonZeidler@users.noreply.github.com>
2023-12-04 15:41:04 +0100

    Updated main.js to include new containers

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
 
 document.getElementById("main-add-id").onclick = mainToAdd;
-
+document.getElementById("normalo-main-logout-id").onclick = mainToLogin;
 document.getElementById("main-logout-id").onclick = mainToLogin;
 
 function mainToAdd() {
     document.getElementById("login-container-id").style.display = "none";
     document.getElementById("main-container-id").style.display = "none";
+    document.getElementById("normalo-main-container-id").style.display = "none";
     document.getElementById("add-container-id").style.display = "";
     document.getElementById("delete-update-container-id").style.display = "none";
+    document.getElementById("details-container-id").style.display = "none";
 }
 
 function mainToLogin() {
     document.getElementById("login-container-id").style.display = "";
     document.getElementById("main-container-id").style.display = "none";
+    document.getElementById("normalo-main-container-id").style.display = "none";
     document.getElementById("add-container-id").style.display = "none";
     document.getElementById("delete-update-container-id").style.display = "none";
+    document.getElementById("details-container-id").style.display = "none";
 }
