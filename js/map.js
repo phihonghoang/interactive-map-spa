5065b39e9e708fe8ce5a3127c1e89373b8681f19
LeonZeidler <147526502+LeonZeidler@users.noreply.github.com>
2023-12-04 15:41:27 +0100

    Updated map.js

--- js/map.js
+++ js/map.js
@@ -1,20 +1,65 @@
+// empty object
+let marker = {};
+
+// lat and lon of the last added address.
+let lastAddedLat;
+let lastAddedLon;
+ 
+// lat and lon of the current address.
+// address.addEventListener("click", function {});
+let currentLat;
+let currentLon;
+
 let mapOptions = {
-    center:[52.5187,13.3855],
-    zoom:10
+    center: [52.5187, 13.3855],
+    zoom: 10
 }
 
-let map = new L.map('main-map' , mapOptions);
+
+let map = new L.map('main-map', mapOptions);
 
 let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 map.addLayer(layer);
 
 
-function initMapMarkers() {
-    let marker1 = new L.Marker([hkw1.lat, hkw1.lon]);
-    let marker2 = new L.Marker([hkw2.lat, hkw2.lon]);
-    let marker3 = new L.Marker([hkw3.lat, hkw3.lon]);
+function initMapMarker(lat, lon) {
+    let key = lat + '_' + lon;
 
-    marker1.addTo(map);
-    marker2.addTo(map);
-    marker3.addTo(map);
+    marker[key] = new L.Marker([lat, lon]);
+    marker[key].addTo(map);
+
+    
+    lastAddedLat = lat;
+    lastAddedLon = lon;
+}
+
+function deleteMapMarker(lat, lon) {
+    let key = lat + '_' + lon;
+    map.removeLayer(marker[key]);
+    delete marker[key];
+}
+
+function reqGeoCor(street, zip, city, state, callback) {
+
+    //var adresse = "Rupprechtstraße 10A, 10317 Berlin, Germany";
+    let address = street + ", " + zip + " " + city + ", " + state;
+    let nominatimUrl = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(address)}`;
+
+    // Führe die Anfrage durch
+    fetch(nominatimUrl)
+        .then(response => response.json())
+        .then(data => {
+            if (data.length > 0) {
+                // Extrahiere Längen- und Breitengrade aus der Antwort
+                let lat = data[0].lat;
+                let lon = data[0].lon;
+
+                initMapMarker(lat, lon)
+            } else {
+                console.error('Keine Ergebnisse für die Adresse gefunden.');
+            }
+        })
+        .catch(error => {
+            console.error('Fehler bei der Geokodierung:', error);
+        });
 }
